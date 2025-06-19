be9ff29e595c4433746182e199af1a463fde1258
kaif pathan <kaifpathan1310@gmail.com>
2025-06-19 14:17:12 +0530

    Chapter 6

---
+++ chapter 6/2  if_elifz_else_ladder.py
@@ -0,0 +1,16 @@
+a = int(input("Enter your age: "))
+
+  # If else Statement
+
+if(a>=18):
+    print("you are above the age of consent")
+    print("good for you")
+  
+
+else:
+    print("you are below the age of consent")
+    
+     
+print("End of program")
+    
+    
---
+++ chapter 6/3 multiple_if_statements.py
@@ -0,0 +1,28 @@
+a = int(input("Enter your age: "))
+
+# If statement no:1
+if(a%2 == 0):
+    print("a is even")
+
+
+# End of If statement no:1    
+
+#If statement no:2
+if(a>=18):
+    print("you are above the age of consent")
+    print("good for you")
+
+elif(a<0):
+    print("you are entering an invalid negative age")
+  
+    
+else:
+    print("you are below the age of consent")
+    
+
+# End of If statement no:2
+    
+     
+print("End of program")
+    
+    
