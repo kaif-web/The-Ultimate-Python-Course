bb399d3b3aad7662a44dd5a5faf4ecafa474002e
kaif pathan <kaifpathan1310@gmail.com>
2025-07-17 13:33:11 +0530

    Chapter 7

---
+++ chapter 7/for_loops.py
@@ -0,0 +1,14 @@
+# for loop whit lists
+l = [1, 4, 6, 234, 6, 764]
+for i in l:
+    print(i)
+ 
+ # for loop with tuples   
+t = (6, 231, 75, 122)
+for i in t:
+    print(i)
+
+# for loop with strings  
+s = "kaif"
+for i in s:
+    print(i)
